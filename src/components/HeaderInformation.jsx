4bcfdec62b6b8317656d3b727d158496e8ff07d1
Gustavo <bachicarp3@gmail.com>
2024-06-05 21:33:09 -0300

    eight commit

--- src/components/HeaderInformation.jsx
+++ src/components/HeaderInformation.jsx
@@ -2,11 +2,11 @@ import pnglogo from "../helpers/logo-word.png";
 import Buttons from "./Buttons";
 const HeaderInformation = () => {
   return (
-    <div className="imagotipo w-full md:w-[500px]">
+    <div className="imagotipo w-full md:w-[500px] mt-[100%] sm:mt-0">
       <div className=" min-w-[500px] lg:gap-16 px-20">
         <img src={pnglogo} alt="Imagotipo" />
       </div>
-      <Buttons firstBtn="Play" SecondBtn="Ver información" />
+      <Buttons firstBtn="Play" SecondBtn="Ver información" url="https://www.youtube.com/watch?v=9m9yRauMJIQ&t=47s" />
     </div>
   );
 };
